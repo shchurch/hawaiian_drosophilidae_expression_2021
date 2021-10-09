7eb9262c1b83b8cf625ce6d4ad06cb698f474595
Samuel Church <samuelchurch@vpn172022154047.its.yale.internal>
2021-10-09 15:13:44 -0600

    update to shiny app

--- app.R
+++ app.R
@@ -77,60 +77,62 @@ server <- function(input, output) {
   output$qplot <- renderPlot({
 
 	target <- input$variable
-  target_genetree <- genetree_all_dmel_id %>% filter(name == target) %>% pull(genetree) %>% unique
-  ovary_changes_cor <- left_join(ovary_changes,data.frame(key = names(full_cormat[target_genetree,]),
-  															correlation = full_cormat[target_genetree,]),by="key") %>% 
-  							na.omit
-  bias_colors <- setNames(c("red","blue"),c("ovary","carcass"))
+  if(target %in% genetree_all_dmel_id$name){
+	  target_genetree <- genetree_all_dmel_id %>% filter(name == target) %>% pull(genetree) %>% unique
+	  ovary_changes_cor <- left_join(ovary_changes,data.frame(key = names(full_cormat[target_genetree,]),
+	  															correlation = full_cormat[target_genetree,]),by="key") %>% 
+	  							na.omit
+	  bias_colors <- setNames(c("red","blue"),c("ovary","carcass"))
 
-	plot_exp <- ggplot(a %>% filter(key == target_genetree),aes(y=factor(sp_code,levels=species_codes[species_order]),x=val,color = bias)) + geom_point(size=3) +
-		scale_color_manual(values = bias_colors) + 
-		geom_vline(xintercept=0,linetype="dashed",size=0.5) + 
-		scale_x_continuous(limits = c(-5,5)) +
-		ggtitle("ovary:carcass bias") +
-		theme(plot.title = element_text(hjust = 0.5)) +
-		theme(text = element_text(size=15)) +
-		theme(axis.title.y = element_blank()) +
-		theme(axis.title.x = element_blank()) +
-		theme(legend.position="none") 
+		plot_exp <- ggplot(a %>% filter(key == target_genetree),aes(y=factor(sp_code,levels=species_codes[species_order]),x=val,color = bias)) + geom_point(size=3) +
+			scale_color_manual(values = bias_colors) + 
+			geom_vline(xintercept=0,linetype="dashed",size=0.5) + 
+			scale_x_continuous(limits = c(-5,5)) +
+			ggtitle("ovary:carcass bias") +
+			theme(plot.title = element_text(hjust = 0.5)) +
+			theme(text = element_text(size=15)) +
+			theme(axis.title.y = element_blank()) +
+			theme(axis.title.x = element_blank()) +
+			theme(legend.position="none") 
 
 
-	correlation_threshold <- 0.825
-	target_changes <- ovary_changes_cor %>% filter(abs(correlation) > correlation_threshold)
-	target_cormat <- target_changes %>% select(name,child_node,scaled_change) %>% 
-		spread(.,name,scaled_change)  %>% 
-		select(-child_node) %>% 
-		cor(.,method="pearson",use="pairwise.complete.obs")
+		correlation_threshold <- 0.825
+		target_changes <- ovary_changes_cor %>% filter(abs(correlation) > correlation_threshold)
+		target_cormat <- target_changes %>% select(name,child_node,scaled_change) %>% 
+			spread(.,name,scaled_change)  %>% 
+			select(-child_node) %>% 
+			cor(.,method="pearson",use="pairwise.complete.obs")
 
-	node_order <- c("13","22","14","23","20","21","15","16","17","19","18","3","2","1","6","5","4","12","11","10","9","8","7")
+		node_order <- c("13","22","14","23","20","21","15","16","17","19","18","3","2","1","6","5","4","12","11","10","9","8","7")
 
-	plot_tree <- grid::rasterGrob(png::readPNG("figures_and_panels/labeled_ultrametric_tree-01.png"))
-	plot_changes <- ggplot(ovary_changes_cor,aes(x=factor(child_node,levels=node_order),y=scaled_change,group=name,color=correlation)) +
-		viridis::scale_color_viridis(option = "B") + 
-		geom_jitter(size=1.5,pch=16,aes(alpha=abs(correlation))) + 
-		geom_point(data=ovary_changes %>% filter(name == target),color="white",fill="black",size=3,stroke=1,pch=21) + 
-		theme(legend.position="none") + 
-		theme(text = element_text(size=10)) +
-		ylab("scaled evolutionary\nchange") + 
-		xlab("phylogenetic branch")
+		plot_tree <- grid::rasterGrob(png::readPNG("figures_and_panels/labeled_ultrametric_tree-01.png"))
+		plot_changes <- ggplot(ovary_changes_cor,aes(x=factor(child_node,levels=node_order),y=scaled_change,group=name,color=correlation)) +
+			viridis::scale_color_viridis(option = "B") + 
+			geom_jitter(size=1.5,pch=16,aes(alpha=abs(correlation))) + 
+			geom_point(data=ovary_changes %>% filter(name == target),color="white",fill="black",size=3,stroke=1,pch=21) + 
+			theme(legend.position="none") + 
+			theme(text = element_text(size=10)) +
+			ylab("scaled evolutionary\nchange") + 
+			xlab("phylogenetic branch")
 
-	tarmat <- target_cormat
-	tarmat[tarmat < 10] <- NA 
-	tarmat[target,] <- target_cormat[target,]
-	tarmat[,target] <- target_cormat[,target]
-	tarmat[target,target] <- NA
+		tarmat <- target_cormat
+		tarmat[tarmat < 10] <- NA 
+		tarmat[target,] <- target_cormat[target,]
+		tarmat[,target] <- target_cormat[,target]
+		tarmat[target,target] <- NA
 
-	plot_network <- as.ggraph(qgraph(mar=c(2,2,2,2),border.width=0.1,tarmat,maximum=1,layout="circle",esize=10,posCol=cor_color_range[3],
-		negCol=cor_color_range[2],labels=colnames(tarmat),color="white",
-		label.font=2,label.scale=F))
+		plot_network <- as.ggraph(qgraph(mar=c(0.2,0.2,0.2,0.2),border.width=0.1,tarmat,maximum=1,layout="circle",esize=10,posCol=cor_color_range[3],
+			negCol=cor_color_range[2],labels=colnames(tarmat),color="white",
+			label.font=2,label.scale=F))
 
-	layout <- rbind(c(1,3),c(2,3),c(4,4),c(5,5),c(6,6),c(6,6))
+		layout <- rbind(c(1,3),c(2,3),c(4,4),c(5,5),c(6,6),c(6,6))
 
-	t1 <- grid::textGrob("Above is the phylogeny of the 12 species\nstudied here, labeled by phylogenetic branch.\n\nTo the right shows the average expression\nratio between the ovary and\ncarcass for each species\nRed=more ovary expression\nBlue=more carcass epxression.",just="left",x=0,y=0.6)
-	t2 <- grid::textGrob("Above shows the distribution of evolutionary changes along each branch in the phylogeny.\nThe black point indicates the selected gene family. Colored points are changes\nin other gene families.\nBelow is the network of genes with a strong evolutionary correlation of expression to the selected gene family.\nFor both panels, orange=strong positive correlation, purple=strong negative correlation.",just="left",x=0,y=0.5)
+		t1 <- grid::textGrob("Above is the phylogeny of the 12 species studied here,\nlabeled by phylogenetic branch.\n\nTo the right shows the average expression ratio\nbetween the ovary and carcass for each species\nRed=more ovary expression\nblue=more carcass epxression.",just="left",x=0,y=0.6)
+		t2 <- grid::textGrob("Above shows the distribution of evolutionary changes along each branch in the phylogeny.\nThe black point indicates the selected gene family. Colored points are changes\nin other gene families.\nBelow is the network of genes with a strong evolutionary correlation of expression to the selected gene family.\nFor both panels, orange=strong positive correlation, purple=strong negative correlation.",just="left",x=0,y=0.5)
 
-	grid.arrange(grobs = list(plot_tree,t1,plot_exp,plot_changes,t2,plot_network), layout_matrix = layout)
-  },height=1000,width=500)
+		grid.arrange(grobs = list(plot_tree,t1,plot_exp,plot_changes,t2,plot_network), layout_matrix = layout)
+		}
+  },height=1100,width=600)
 
 }
 
